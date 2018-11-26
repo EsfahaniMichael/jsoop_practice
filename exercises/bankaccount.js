1f4def73aae5c12dcff5706c79012a715f113ea5
Esfahani <esfahanimichael@gmail.com>
2018-11-25 22:58:46 -0800

    finished for the night

--- exercises/bankaccount.js
+++ exercises/bankaccount.js
@@ -1,23 +1,38 @@
 
-
 class Account{
-	constructor(){
-		//store the amount of money in the account
-	}
-	add(  ){
-		//add money to the amount stored in the account
-		//takes in an amount
-		//adds it to the existing amount
-		//returns the new amount in the account
-	}
-	remove( amount ){
-		//removes funds from an account
-		//check if the amount to withdraw is more than the account
-		//if more, only withdraw the amount in the account, not more
-		//if less, withdraw the amount specified
-		//return the amount actually withdrawn
-	}
-	getAmount(){
-		//returns the amount in the account
-	}
+    constructor(){
+        this.money = null;
+        //store the amount of money in the account
+    }
+    add( amount ){
+        this.money += amount;
+        return this.money;
+        //add money to the amount stored in the account
+        //takes in an amount
+        //adds it to the existing amount
+        //returns the new amount in the account
+    }
+    remove( amount ){
+        var withdrawn = null;
+
+        if (this.money > amount){
+
+            this.money = this.money - amount;
+            withdrawn = amount;
+        }
+        else if (this.money < amount){
+            withdrawn = this.money;
+            this.money = this.money - this.money;
+        }
+        return withdrawn;
+        //removes funds from an account
+        //check if the amount to withdraw is more than the account
+        //if more, only withdraw the amount in the account, not more
+        //if less, withdraw the amount specified
+        //return the amount actually withdrawn
+    }
+    getAmount(){
+        return this.money;
+        //returns the amount in the account
+    }
 }
--- exercises/basiccalcobject.js
+++ exercises/basiccalcobject.js
@@ -1,30 +1,80 @@
 
 class Calculator{
-	constructor(){
-		//takes in nothing
-		//make storage for the operator and the numbers
-	}
-	loadOperator(  ){
-		//adds the operator to the next calculation
-		//takes in the operator
-		//checks if it is a valid operation (+-*/)
-			//save the op to the constructor storage
-			//return true
-		//or return false if not the right operator
-	}
-	loadNumber(  ){
-		//takes in a number and stores it as one of the numbers to perform math on
-		//takes in 1 number
-		//checks if it is actually a number and if we have fewer than 2 numbers
-		//if it is a number, and we have 2 or fewer numbers, store it
-			//return the number of numbers stored
-		//otherwise return false (too many numbers stored)
-	}
-	calculate(){
-		//calculate the result of the stored numbers and operator
-		//takes in nothing
-		//calculates with the operator and 2 numbers
-		//clears out the stored numbers
-		//returns the calculated result
-	}
+    constructor(){
+        this.operator = null;
+        this.number = {
+
+        }
+
+        //takes in nothing
+        //make storage for the operator and the numbers
+    }
+    loadOperator(  operator){
+        if (operator === '+' || '-' || '*' || '/'){
+            this.operator = operator;
+            return true;
+        }
+        else{
+            return false;
+        }
+        //adds the operator to the next calculation----------***
+        //takes in the operator
+        //checks if it is a valid operation (+-*/)
+        //save the op to the constructor storage
+        //return true
+        //or return false if not the right operator
+    }
+    loadNumber( number ){
+
+        if (typeof(number ) === 'string'){
+            return false;
+        }
+        else if(typeof(number) === 'number' && (!this.number.numberOne)){
+            this.number.numberOne = number;
+            var numLength = Object.keys(this.number);
+            return numLength.length;
+        }
+        else if (typeof(number) === 'number' && (!this.number.numberTwo)){
+            this.number.numberTwo = number;
+            numLength = Object.keys(this.number);
+            return numLength.length;
+        }
+        else if ((this.number.numberOne) && (this.number.numberTwo)){
+            return false;
+        }
+
+
+        //takes in a number and stores it as one of the numbers to perform math on
+        //takes in 1 number
+        //checks if it is actually a number and if we have fewer than 2 numbers
+        //if it is a number, and we have 2 or fewer numbers, store it
+        //return the number of numbers stored
+        //otherwise return false (too many numbers stored)
+    }
+    calculate(){
+        var answer = null;
+        var num1 = parseInt(this.number.numberOne);
+        var num2 = parseInt(this.number.numberTwo);
+        if (this.operator == '+'){
+            answer = num1 + num2;
+        }
+        else if (this.operator == '-'){
+            answer = num1 - num2;
+        }
+        else if (this.operator == '*'){
+            answer = num1 * num2;
+        }
+        else if (this.operator == "/"){
+            answer = (num1 / num2);
+        }
+
+        this.number = {};
+        return answer;
+
+        //calculate the result of the stored numbers and operator
+        //takes in nothing
+        //calculates with the operator and 2 numbers
+        //clears out the stored numbers
+        //returns the calculated result
+    }
 }
